03826690f7c721878c01ba56823fc5044c8ca5a6
stephanedony <stephane.dony@icapps.com>
2024-09-03 10:55:03 +0200

    Chore: Add styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
+import {TodolistComponent} from "./todolist/todolist.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  // { path: 'todos', component: HomeComponent },
+  { path: 'todos', component: TodolistComponent },
 ];
 
 @NgModule({
---
+++ src/app/todolist/ITodo.ts
@@ -0,0 +1,6 @@
+export interface ITodo  {
+  id: number;
+  title: string;
+  creationDate: string;
+  isDone: boolean;
+}
